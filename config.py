d23d6458fc752e1bb0dce5c60c98df7cd792121f
HewoLoser <71967962+HewoLoser@users.noreply.github.com>
2020-09-28 00:11:38 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
-token = ""
+token = "NzU5OTk1MjE2NTI1Nzg3MTU3.X3Fmsw.tYUANtccQbog85ricqaVKRZXpO4"
 # the id of the chat to send bot logs of admin value i.e
 # (kick, ban, purge, etc)
-admin_actions_log_channel_id = 741923499542839330
+admin_actions_log_channel_id = 759869978475888704
 # the id of the chat to send bot logs of like general commands that were
 # executed
-general_actions_log_channel_id = 741923499542839330
+general_actions_log_channel_id = 759869978475888704
 # the name of the role the user should get once theyve been verified
 verify_role_name = 'Verified'
 # the prefix to be used
